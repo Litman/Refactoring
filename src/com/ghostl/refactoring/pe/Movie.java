54bd09207b555a22a788d1156430226890488fec
layala <layala@vectoritcgroup.com>
2017-08-31 15:47:59 -0500

    "finish first program"

--- src/com/ghostl/refactoring/pe/Movie.java
+++ src/com/ghostl/refactoring/pe/Movie.java
@@ -2,4 +2,29 @@ package com.ghostl.refactoring.pe;
 
 public class Movie {
 
+	public static final int CHILDRENS = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
+	
+	private String mTitle;
+	private int mPriceCode;
+	
+	public Movie(String title, int priceCode) {
+		mTitle = title;
+		mPriceCode = priceCode;
+	}
+
+	public int getmPriceCode() {
+		return mPriceCode;
+	}
+
+	public void setmPriceCode(int mPriceCode) {
+		this.mPriceCode = mPriceCode;
+	}
+	
+	public String getmTitle() {
+		return mTitle;
+	}
+	
+	
 }
