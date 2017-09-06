2b161a81968122667dafc027deed41b2adac6228
layala <layala@vectoritcgroup.com>
2017-09-06 16:38:28 -0500

    removed variable totalPointFrequent getTotalFrequentRenterPoints and add method htmlStatement

--- src/com/ghostl/refactoring/pe/Customer.java
+++ src/com/ghostl/refactoring/pe/Customer.java
@@ -20,29 +20,34 @@ public class Customer {
 	}
 	
 	public String statement(){
-		int frequentRenterPoints = 0;
 		Enumeration rentals = mRentals.elements();
 		String result = "Rental Record for "+ getmName() + "\n";
 		while(rentals.hasMoreElements()){
 			Rental each = (Rental) rentals.nextElement();
-			
-			frequentRenterPoints += each.getFrequentRenterPoints();
 			
 			//show figures for this rental
 			result += "\t" + each.getmMovie().getmTitle() + "\t" +
 					String.valueOf(each.getCharge() + "\n");
 		}
 		//add footer lines
-		result += "Amount owed is " + String.valueOf(geTotalCharge()) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
 		
 		return result;
 		
 	}
 	
-	
-	
-	private double geTotalCharge() {
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		Enumeration rentals = mRentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			result =+ each.getFrequentRenterPoints();
+		}
+		return result;
+	}
+
+	private double getTotalCharge() {
 		double result = 0;
 		Enumeration rentals = mRentals.elements();
 		while (rentals.hasMoreElements()){
@@ -78,5 +83,21 @@ public class Customer {
 			break;
 		}
 		return result;*/
+	}
+	
+	public String htmlStatement(){
+		Enumeration rentals = mRentals.elements();
+		String result = "<H1> Rentals for <EM> " + getmName() + "</EM> </H1> <P>\n";
+		while(rentals.hasMoreElements()){
+			Rental each = (Rental)rentals.nextElement();
+			
+			//Show figures for each rental
+			result += each.getmMovie().getmTitle() + ": "+ String.valueOf(each.getCharge()) + "<BR>\n";
+			
+		}
+		//add footer lines
+		result += "<P> You owe <EM>" + String.valueOf(getTotalCharge())+ "</EM><P>\n";
+		result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints())+ "</EM> frequent renter points <P>";
+		return result;
 	}
 }
