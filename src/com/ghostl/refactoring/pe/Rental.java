838287c3e21f2d9dad2db78f0c3cf73fbad00dcd
layala <layala@vectoritcgroup.com>
2017-09-06 18:16:10 -0500

    Changes to move getFrequentRenerPonits to object Movie

--- src/com/ghostl/refactoring/pe/Rental.java
+++ src/com/ghostl/refactoring/pe/Rental.java
@@ -20,36 +20,11 @@ public class Rental {
 	
 
 	double getCharge(){//TODO Move method amountFor to a correct object
-		double result = 0; 
-		switch (getmMovie().getmPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if(getmDaysRented() > 2){
-				result += (getmDaysRented() - 2 ) * 1.5;
-			}				
-			break;
-		case Movie.NEW_RELEASE:
-			result += getmDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if(getmDaysRented() > 3){
-				result += (getmDaysRented() - 3) * 1.5;					
-			}
-			
-		default:
-			break;
-		}
-		return result;
+		return mMovie.getCharge(mDaysRented);
 	}
 	
 	int getFrequentRenterPoints(){
-		if((getmMovie().getmPriceCode() == Movie.NEW_RELEASE) && getmDaysRented() > 1){
-			return 2;
-		} 
-		else{
-			return 1;
-		}
+		return mMovie.getFrequentRenterPoints(mDaysRented);
 	}
 	
 }
